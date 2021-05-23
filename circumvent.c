57f6e330ff45e6bb9dd47902aabbf7b0e37b7b1b
zseri <zseri.devel@ytrizja.de>
2021-05-23 02:09:51 +0200

    try to generate a decryption table...

--- circumvent.c
+++ circumvent.c
@@ -1,23 +1,29 @@
 #define _GNU_SOURCE
 #include <inttypes.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
 #include <dlfcn.h>
+#include <sys/wait.h>
 
 // ptrace spamming is too intense, unable to mitigate...
-//long ptrace(int request, int pid, void *addr, void *data) {
+long ptrace(int request, int pid, void *addr, void *data) {
   //printf("ptrace: %d %d ...\n", request, pid);
   //write(1, ".", 1);
-  //return 0;
-//}
+  return 0;
+}
+
+pid_t waitpid(pid_t pid, int *wstatus, int options) {
+  printf("waitpid\n");
+  return -1;
+}
 
 int raise(int signal) {
   printf("raise: %d\n", signal);
   return 0;
 }
 
-/*
 int clock_gettime(clockid_t clockid, struct timespec * tp) {
   printf("clock_gettime\n");
   return 0;
@@ -27,14 +33,11 @@ int clock_settime(clockid_t clockid, const struct timespec * tp) {
   printf("clock_settime\n");
   return 0;
 }
-*/
-
 
 static void _y_print_xy(const char *what, const char *s) {
   printf("  %s = %.*s\n", what, 100, s);
 }
 
-/*
 int inotify_add_watch(int fd, const char * pathname, uint32_t mask) {
   printf("inotify_add_watch %d, \"%.100s\", %d\n", fd, pathname, mask);
   _y_print_xy("path", pathname);
@@ -46,7 +49,6 @@ int access(const char * pathname, int mode) {
   _y_print_xy("path", pathname);
   return 0;
 }
-*/
 
 int strncmp(const char *s1, const char *s2, size_t n) {
   printf("strncmp... n = %zu\n", n);
